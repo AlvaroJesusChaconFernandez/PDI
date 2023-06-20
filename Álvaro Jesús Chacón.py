2ce93000a12c4f07711bfffe00c2de9fa8ca27b4
AlvaroJesusChaconFernandez <137121336+AlvaroJesusChaconFernandez@users.noreply.github.com>
2023-06-20 08:23:51 -0400

    Update Álvaro Jesús Chacón.py

--- Álvaro Jesús Chacón.py
+++ Álvaro Jesús Chacón.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('imagen.jpg',0)
+img = cv2.imread('00015.DCM',0)
 edges = cv2.Canny(img,100,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
